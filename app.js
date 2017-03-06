389fc1f8311b93a2917ab124094f4a6087081d50
Andy Lui <andylui@Andys-MBP.home>
2017-03-05 21:58:24 -0800

    heroku fix

--- app.js
+++ app.js
@@ -11,13 +11,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set("port", process.env.PORT || 3000);
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRoutes);
 app.get('/', function(req, res, next) {
 	res.status(200);

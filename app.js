7d42876b8c64c43b439b051b3dc2fb049ac661b7
Andy Lui <andylui@Andys-MBP.home>
2017-03-05 23:01:42 -0800

    cors

--- app.js
+++ app.js
@@ -7,10 +7,18 @@ var bodyParser = require('body-parser');
 var apiRoutes = require('./routes/index');
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set("port", process.env.PORT || 3000);
+app.use(allowCrossDomain);
 
 
 app.use(logger('dev'));

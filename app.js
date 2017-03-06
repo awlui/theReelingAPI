67c09dea6ba0d1395a2c122ea440e9c32da91160
Andy Lui <andylui@Andys-MBP.home>
2017-03-05 21:54:14 -0800

    adjust for heroku push

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.set("port", process.env.PORT || 3000);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -41,4 +41,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// module.exports = app;
+
+app.listen(app.get("port"), function() {
+  console.log("Server started on port " + app.get('port'));
+});
